f70206910ab82b9fd06b1d3f6e6988461011cf3f
William Scotten <wscotten@MacPros-MacBook-Pro.local>
2017-08-07 09:50:56 -0700

    added text to bar component

--- src/app/components/Bar.js
+++ src/app/components/Bar.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const BarText = ({ color = '#333', text }) =>
+  (<p
+    style={{
+      textAlign: 'center',
+      marginTop: 5,
+      marginBottom: 5,
+      color,
+    }}
+  >{text}
+  </p>);
+
+BarText.propTypes = {
+  color: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
 const Bar = ({ topText, bottomText, height, backgroundColor }) =>
   (<div>
-    <p style={{ color: backgroundColor }}>{topText}</p>
+    <BarText color={backgroundColor} text={topText} />
     <div
       style={{
         width: 30,
@@ -12,7 +28,7 @@ const Bar = ({ topText, bottomText, height, backgroundColor }) =>
         backgroundColor,
       }}
     />
-    <p>{bottomText}</p>
+    <BarText text={bottomText} />
   </div>);
 
 Bar.propTypes = {
