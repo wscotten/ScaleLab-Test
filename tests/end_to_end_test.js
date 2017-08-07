6f696ed305c4711af89b42c830ba88ecdd7172c8
William Scotten <wscotten@MacPros-MacBook-Pro.local>
2017-08-06 20:59:10 -0700

    updated nightwatch test

--- tests/end_to_end_test.js
+++ tests/end_to_end_test.js
@@ -2,7 +2,7 @@ module.exports = {
   'End to End Test Project': (browser) => {
     browser
       .url('http://localhost:3000/')
-      .waitForElementPresent('.channel-stats-container', 1000)
+      .waitForElementPresent('.channel-stats-container', 2000)
       .click('.detailsButton')
       .waitForElementNotPresent('.channel-stats-container', 1000)
       .click('.detailsButton')
@@ -18,7 +18,10 @@ module.exports = {
       .waitForElementPresent('.modal-box', 1000)
       .setValue('.modal-contents:nth-of-type(3) input', 1)
       .click('.modal-button')
-      .waitForElementNotPresent('.modal-box', 3000)
+      .waitForElementPresent('.put-result-status-container', 3000)
+      .moveToElement('.modal-container', 10, 10)
+      .mouseButtonClick(0)
+      .waitForElementNotPresent('.modal-box', 1000)
       .getText('.social-reach-bars:nth-of-type(1)', function(result) {
         this.assert.equal(result.value, '9.9M');
       })
