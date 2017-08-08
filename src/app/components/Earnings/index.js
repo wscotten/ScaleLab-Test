7f2b1de61f5532bc150a10d45799d8458d166d84
William Scotten <wscotten@MacPros-MacBook-Pro.local>
2017-08-07 23:01:17 -0700

    fixed duplicate slice logic in earnings

--- src/app/components/Earnings/index.js
+++ src/app/components/Earnings/index.js
@@ -19,7 +19,7 @@ const style = ({ barColor, monthsToShow, earnings }) => {
     <div className={'earnings-container'}>
       <h4>Earnings</h4>
       <div className={'earnings-bar-container'}>
-        {earnings.slice(earnings.length - monthsToShow).map(({ month, gross, id }) =>
+        {earnings.map(({ month, gross, id }) =>
           (<Bar
             key={id}
             topText={`$${gross.toLocaleString()}`}
