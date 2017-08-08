2dc9bc01a53f0b4cee294bfb5793269c8482a713
William Scotten <wscotten@MacPros-MacBook-Pro.local>
2017-08-07 20:47:37 -0700

    hooked up redux devtools extension

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -11,18 +11,24 @@ import reducers from './app/RootReducer';
 import registerServiceWorker from './registerServiceWorker';
 import getJSONFromApi from './ApiCommunication';
 
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+
 const middlewares = [
   thunk,
 ];
 
 if (process.env.NODE_ENV === 'development') {
+  /* eslint-disable import/no-extraneous-dependencies, global-require */
   const { logger } = require('redux-logger');
+  /* eslint-enable */
   middlewares.push(logger);
 }
 
 const store = createStore(
   reducers,
-  applyMiddleware(...middlewares),
+  composeEnhancers(applyMiddleware(...middlewares)),
 );
 
 (async function wait() {
